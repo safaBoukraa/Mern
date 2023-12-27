160e9c8430da363263a8b2081a27ba55d0e5bb39
unknown <safaboukraa5@gmail.com>
2023-12-27 16:25:04 +0100

    API

---
+++ week_2/Day_1/Practice/Pokemon api/my-app/src/App.js
@@ -0,0 +1,30 @@
+
+import './App.css';
+import {useState} from 'react'
+
+function App() {
+  const [pokemon, setPokemon] = useState([]);
+
+  const fetchPokemon = () => {
+    fetch("https://pokeapi.co/api/v2/pokemon/") 
+      .then((serverResponse) => serverResponse.json())
+      .then((jsonResponse) => setPokemon(jsonResponse.results)) 
+      .catch((error) => console.log("Something went wrong ", error)); 
+      console.log(pokemon);
+  };
+  return (
+    <div className="App">
+
+      <button className='btn' onClick ={fetchPokemon} >Fetch Pokemon</button>
+  
+        {pokemon.map((onepokemon,id)=>{
+          return <p key={id}>{onepokemon.name}</p>
+        })}
+
+  
+
+    </div>
+  );
+}
+
+export default App;
